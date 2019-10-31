7fbf3090c4ecba6c84e59634c148b23ec4daa740
rednaxela700 <ohleg@windowslive.com>
2019-10-31 20:01:20 +0100

    using the component to display a groceries list

--- component.js
+++ component.js
@@ -9,3 +9,14 @@ Vue.component('todo-item', {
     props: ['todo'],
     template: '<li>{{todo.text}}</li>'
 });
+
+var app7 = new Vue({
+    el: '#app7',
+    data: {
+        groceryList: [
+            {id: 0, text: 'Vegetables'},
+            {id: 1, text: 'Fruits'},
+            {id: 2, text: 'Cheese'}
+        ]
+    }
+});
