59c7fa1225d8aa47b107fb7ae2b76cf1e87cfb18
rednaxela700 <ohleg@windowslive.com>
2019-10-31 14:17:57 +0100

    created a list and append li children with text added app3 which can disappear on seen = false

--- index.js
+++ index.js
@@ -17,6 +17,32 @@ const app2 = new Vue({
     }
 });
 
+const app3 = new Vue({
+    el: '#app3',
+    data: {
+        seen: true
+    }
+});
+
+const app4 = new Vue({
+    el: '#app4',
+    data: {
+        todos: [
+            {text: 'Learn JS'},
+            {text: 'Learn Vue'},
+            {text: 'Build sth awesome'}
+        ]
+
+
+    }
+});
+
+//append new item to todo list
+app4.todos.push({text: 'Keep order in arr'});
+app4.todos.push({text: 'Keep fucking order in arr'});
+app4.todos.push({text: 'Keep fucking order in fucking arr'});
+app4.todos.push({text: 'Keep fucking order in fucking arr for fuck sake'});
+
 
 
 app();
